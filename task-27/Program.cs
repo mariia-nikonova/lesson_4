9559ebe878fd1004cc975306f4ec35ec7e8701c3
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-08-01 12:37:19 +0300

    task-27_commit

---
+++ task-27/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+int tmp = 0;
+int result = 0;
+Console.Write($"Сумма цифр числа {a}: ");
+
+int sum(int c)
+{
+    if (a < 10) Console.Write($"Сумма цифр числа: {a}");
+    else
+        while (a / 10 != 0)
+        {
+            tmp = a % 10;
+            a = a / 10;
+            result = result + tmp;
+        }
+    result = result + a;
+    return result;
+}
+int s = sum(a);
+Console.Write(s);
