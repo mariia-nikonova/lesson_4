60018470f30e6fb7ffa0f3d93f208a4705f2be03
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-08-01 13:42:43 +0300

    task-29_commit

---
+++ task-29/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rand = new Random();
+int s = 8;
+Console.Write("[ ");
+
+int[] myArray (int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(40);
+        Console.Write($"{array[i]} ");
+    }
+    return array;
+}
+int[] myArr = myArray(s);
+Console.Write("]");
