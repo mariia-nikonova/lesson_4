35d1c17f34a93a1bb871deedbedfa177c2d1b54f
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-08-01 12:02:11 +0300

    task-25_commit

---
+++ task-25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число А: ");
+double a = double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите натуральное число В: ");
+int b = int.Parse(Console.ReadLine()!);
+double result = 1;
+double power (double c, int d) 
+{   for (int i=1;i<=b;i++)
+    {
+        result=a*result;
+    }
+    return result;
+}
+double p=power(a,b);
+Console.Write($"Число А в степени В равно: {p}");
